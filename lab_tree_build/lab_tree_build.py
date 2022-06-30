64a3e0cc9626b49f11cdfc11e43b5e70ce246a21
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-06-30 12:21:40 -0700

    can create VE

--- lab_tree_build/lab_tree_build.py
+++ lab_tree_build/lab_tree_build.py
@@ -31,6 +31,8 @@ def create_test_file(file_name):
         pass
     with open('test/__init__.py', 'w') as f:
         pass
+def create_venv():
+    os.system('python3 -m venv .venv') 
 
 def Main():
     
@@ -39,7 +41,7 @@ def Main():
     create_gitignore()
     create_readme()
     create_test_file(file_name)
+    create_venv()
     closing_msg()
-    
 
 Main()
