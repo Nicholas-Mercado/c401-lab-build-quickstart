ed03e7b47eb79a7909e569fcdef82080351dc3a5
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-06-30 12:17:09 -0700

    can create readme

--- lab_tree_build/lab_tree_build.py
+++ lab_tree_build/lab_tree_build.py
@@ -11,9 +11,7 @@ def create_project_file(file_name):
     os.makedirs(f'{file_name}')
     with open(f'{file_name}/{file_name}.py', 'w') as f:
         pass
-    
-    
-    
+        
 def create_gitignore():
     """Creates gitignore file from assets"""
     with open(f'{get_path()}/file_schema/get_gitignore.txt', 'r') as input:
@@ -21,9 +19,19 @@ def create_gitignore():
             for line in input:
                 output.write(line)
 
+def create_readme():
+    with open(f'{get_path()}/file_schema/get_README.txt', 'r') as input:
+        with open("README.md", "w") as output:
+            for line in input:
+                output.write(line)
+
 def Main():
     
     file_name = opening_msg()
     create_project_file(file_name)
+    create_gitignore()
+    create_readme()
     closing_msg()
+    
+
 Main()
